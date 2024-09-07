092802174773f21a037d55a675da3e077c03617f
iskkang <76502863+iskkang@users.noreply.github.com>
2024-09-08 05:31:23 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const priceRoutes = require('/routes/priceRoutes');
+const priceRoutes = require('./routes/priceRoutes');
 
 const app = express();
 
