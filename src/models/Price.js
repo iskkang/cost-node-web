933c78fc09a80ad7393dd73edfd3d83bb0614a68
iskkang <76502863+iskkang@users.noreply.github.com>
2024-09-08 04:03:47 +0900

    Update Price.js

--- src/models/Price.js
+++ src/models/Price.js
@@ -1,31 +1,63 @@
 const airtableClient = require('../utils/airtableClient');
 
 class Price {
-    static async getPriceInfo(pol, pod, type) {
-    const safePol = pol.replace(/'/g, "\\'");
-    const safePod = pod.replace(/'/g, "\\'");
-    const safeType = type.replace(/'/g, "\\'");
-  
-    const records = await airtableClient.select({
-      filterByFormula: `AND(
-        LOWER({pol}) = LOWER('${safePol}'),
-        LOWER({pod}) = LOWER('${safePod}'),
-        LOWER({Type}) = LOWER('${safeType}')
-      )`
-    }).firstPage();
-  
-    if (records.length > 0) {
-      const record = records[0];
-      return {
-        pol: record.get('pol'),
-        pod: record.get('pod'),
-        type: record.get('Type'),
-        cost: record.get('cost'),
-        time: record.get('t/Time'),
-        route: record.get('route')
-      };
-    }
-    return null;
+  static async getSuggestions(field, query) {
+    const safefield = field.replace(/[^a-zA-Z0-9]/g, '');
+    const safeQuery = query.replace(/'/g, "\\'").toLowerCase();
+
+    return new Promise((resolve, reject) => {
+      const suggestions = new Set();
+      
+      airtableClient.select({
+        filterByFormula: `LOWER(FIND('${safeQuery}', LOWER({${safefield}}))) > 0`
+      }).eachPage((records, fetchNextPage) => {
+        records.forEach(record => {
+          suggestions.add(record.get(safefield));
+        });
+        fetchNextPage();
+      }, err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(Array.from(suggestions));
+      });
+    });
+  }
+
+  static async getPriceInfo(pol, pod, type) {
+    const safePol = pol.replace(/'/g, "\\'").toLowerCase();
+    const safePod = pod.replace(/'/g, "\\'").toLowerCase();
+    const safeType = type.replace(/'/g, "\\'").toLowerCase();
+
+    return new Promise((resolve, reject) => {
+      airtableClient.select({
+        filterByFormula: `AND(
+          LOWER({POL}) = '${safePol}',
+          LOWER({POD}) = '${safePod}',
+          LOWER({Type}) = '${safeType}'
+        )`,
+        maxRecords: 1
+      }).firstPage((err, records) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        if (records.length > 0) {
+          const record = records[0];
+          resolve({
+            pol: record.get('POL'),
+            pod: record.get('POD'),
+            type: record.get('Type'),
+            cost: record.get('cost'),
+            time: record.get('t/Time'),
+            route: record.get('route')
+          });
+        } else {
+          resolve(null);
+        }
+      });
+    });
   }
 }
 
