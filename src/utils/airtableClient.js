f6a89eef91b1c19937e809aab702bb219388b54f
iskkang <76502863+iskkang@users.noreply.github.com>
2024-09-08 04:03:16 +0900

    Update airtableClient.js

--- src/utils/airtableClient.js
+++ src/utils/airtableClient.js
@@ -1,6 +1,11 @@
 const Airtable = require('airtable');
 
-const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
-const table = base('YOUR_TABLE_NAME');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+
+const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
+const table = base('tcr');  // 'tcr'은 실제 테이블 이름으로 변경해야 합니다.
 
 module.exports = table;
