a22982a057a787ffd33ea7e04d107a5fea9d0da6
iskkang <76502863+iskkang@users.noreply.github.com>
2024-09-08 04:54:13 +0900

    Create suggestionRoutes.js

---
+++ src/routes/suggestionRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const suggestionController = require('../controllers/suggestionController');
+
+// 추천 관련 엔드포인트
+router.get('/suggestions', suggestionController.getSuggestions);
+
+module.exports = router;
