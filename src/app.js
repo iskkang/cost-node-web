684721c491e54443278b1c7f914d0aa31ddccdcf
iskkang <76502863+iskkang@users.noreply.github.com>
2024-09-08 04:01:37 +0900

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const priceRoutes = require('./routes/priceRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', priceRoutes);
+
+module.exports = app;
