5f66b54f1d83668979a59e24af39ef9bf26dff1b
iskkang <76502863+iskkang@users.noreply.github.com>
2024-09-08 05:27:09 +0900

    Update priceRoutes.js

--- src/routes/priceRoutes.js
+++ src/routes/priceRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const priceController = require('./controllers/priceController');
+const priceController = require('../controllers/priceController');
 
 // 가격 제안 및 가격 확인 엔드포인트
 router.get('/suggestions', priceController.getSuggestions);
